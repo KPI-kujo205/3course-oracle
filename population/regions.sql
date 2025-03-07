bac4339b7df9ec99ad367900ce960d2b9e8533f4
kujo205 <kuc8301@gmail.com>
2025-03-07 11:49:48 +0200

    feat: made lab 1

---
+++ population/regions.sql
@@ -0,0 +1,11 @@
+insert into Regions (REGION_ID, REGION_NAME)
+values (1, 'Europe');
+
+insert into Regions (REGION_ID, REGION_NAME)
+values (2, 'Americas');
+
+insert into Regions (REGION_ID, REGION_NAME)
+values (3, 'Asia');
+
+insert into Regions (REGION_ID, REGION_NAME)
+values (4, 'Middle East and Africa');
